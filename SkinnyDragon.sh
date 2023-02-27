f8e27ad5727e60ec15440fbfa7f280298d88c174
Skinny R&D <jason@skinnyrd.com>
2023-02-27 12:59:42 -0600

    Add files via upload

---
+++ SkinnyDragon.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "  ____  _    _                                  "
+echo " / ___|| | _(_)_ __  _ __  _   _                "
+echo " \___ \| |/ / | '_ \| '_ \| | | |               "
+echo "  ___) |   <| | | | | | | | |_| |               "
+echo " |____/|_|\_\_|_| |_|_| |_|\__, |    ___  ____  "
+echo " |  _ \ _ __ __ _  __ _  __|___/__  / _ \/ ___| "
+echo " | | | | '__/ _` |/ _` |/ _ \| '_ \| | | \___ \ "
+echo " | |_| | | | (_| | (_| | (_) | | | | |_| |___) |"
+echo " |____/|_|  \__,_|\__, |\___/|_| |_|\___/|____/ "
+echo "                  |___/                         "
+
+sudo apt-get update
+sudo apt-get install bluez-tools openssh-server
+
+echo
+
+echo "****Create Permanent alias for Blue_Hydra****"
+echo
+cd ~
+echo "alias blue_hydra='sudo ~/blue_hydra/bin/blue_hydra'" > .bash_aliases
+echo
+
+echo
+echo "You will need to assign a password to this user."
+echo "Your username is: live"
+echo "Please follow the steps below to change the password."
+echo "When prompted for the Current Password, just press Enter."
+echo
+
+passwd
